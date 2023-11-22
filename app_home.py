2adee2da28119d5bcc87fe98736b02699d07e148
jeon1700 <wjdtn990107@gmail.com>
2023-11-22 11:39:43 +0900

    main

---
+++ app_home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def run_home_app():
+    st.subheader('Welcome~~')
+    st.text('이 앱은 고객 데이터와 자동차 구매 데이터에 대한 내용입니다.')
+    st.text('고객 정보를 넣으면, 차 구매 금액도 예측해 줍니다.')
+
+
+    img_url ='https://www.motorgraph.com/news/photo/201905/22564_72789_5839.jpg'
+    
+    st.image(img_url)
